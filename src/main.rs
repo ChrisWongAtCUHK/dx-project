35df576124d1dee0437a96df6be892b6136b92a5
Chris Wong <chris.wong.ksl@gmail.com>
2026-04-24 04:23:12 +0800

    Remove FAVICON

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use dioxus::prelude::*;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
@@ -11,7 +10,6 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
         Hero {}
         Echo {}
